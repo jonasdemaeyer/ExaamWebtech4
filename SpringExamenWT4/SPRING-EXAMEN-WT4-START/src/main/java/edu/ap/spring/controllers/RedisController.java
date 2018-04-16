1a57af22222b53825a86c19e0fc7b8721d5c2135
jonasdemaeyer <jonas.demaeyer@hotmail.com>
2018-04-16 15:41:31 +0200

    commit 1 spring

---
+++ SpringExamenWT4/SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/controllers/RedisController.java
@@ -0,0 +1,38 @@
+package edu.ap.spring.controllers;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.ap.spring.redis.RedisService;
+
+public class RedisController {
+
+	private RedisService service;
+	
+	   @Autowired
+		public void setRedisService(RedisService service) {
+			this.service = service;
+	   }
+	   
+	   @RequestMapping("/list")
+	   @ResponseBody
+	   public String list() {
+		   String html = "<HTML>";
+		   
+		   Set<byte[]> studenten = service.setKey(key, html);
+		   Set<byte[]> examen = service.setKey(key, html);
+		   Set<byte[]> reden = service.setKey(key, html);
+		   Set<byte[]> datum = service;
+		   
+		   
+		   
+		   
+	   }
+	   
+	   
+}
